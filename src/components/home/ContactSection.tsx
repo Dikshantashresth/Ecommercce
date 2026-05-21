71866f1417999674c627aaed1bff14fec4504b44
dikshanta-dev <dedit10000@gmail.com>
2026-05-21 08:35:26 +0545

    fix: update phone number formatting in ContactSection and ProductCard components

--- src/components/home/ContactSection.tsx
+++ src/components/home/ContactSection.tsx
@@ -154,7 +154,7 @@ export default function ContactSection() {
                 value={form.phone}
                 onChange={handleChange}
                 className={inputCls}
-                placeholder="+977  986-0783731"
+                placeholder="+977  9860783731"
               />
             </div>
             <div>
